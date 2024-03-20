849ade7666be066bc63ab5f1b509b53234a68c46
Fidelis Adang <fidenki1429@gmail.com>
2024-03-20 15:44:29 +0100

    nav

--- src/app/page.js
+++ src/app/page.js
@@ -6,24 +6,33 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <MaxWidthWrapper>
-      <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
-        <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
-          Your best market place for high-quality{' '}
-          <span className='text-blue-600'>staffing</span>.
-        </h1>
-        <p className='mt-6 text-lg max-w-prose text-muted-foreground'>
-          Welcome to Nursing Empire. Every staff on our selection is verified by
-          our team to ensure our highest quality standards.
-        </p>
-        <div className='flex flex-col sm:flex-row gap-4 mt-6'>
-          <Link href='/products' className={buttonVariants()}>
-            Learn More
-          </Link>
-          <Button variant='ghost'>Our quality promise &rarr;</Button>
+      <div className='py-20 flex flex-col items-center max-w-3xl'>
+        <div className='flex justify-between gap-12'>
+          <div className='relative'>
+            <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
+              Your best market to invest and make{' '}
+              <span className='text-blue-600'>Money</span>while asleep.
+            </h1>
+            <p className='mt-6 text-lg max-w-prose text-muted-foreground'>
+              Welcome to Helping-Hands Investment. Where every member is
+              verified and ready to help each other by combining our resources
+              to create a larger capital for a larger interest for all members.
+            </p>
+            <div className='flex flex-col sm:flex-row gap-4 mt-6'>
+              <Link href='/' className={buttonVariants()}>
+                Register
+              </Link>
+              <Button variant='ghost'>Login &rarr;</Button>
+            </div>
+          </div>
+          <div className='flex flex-col mx-auto items-center'>
+            <p>login or Register</p>
+            <form>
+              <input />
+            </form>
+          </div>
         </div>
       </div>
-
-      {/* TODO List products */}
     </MaxWidthWrapper>
   )
 }
---
+++ src/components/NavItems.jsx
@@ -0,0 +1,27 @@
+'use client'
+
+import Link from 'next/link'
+import { useState } from 'react'
+import { buttonVariants } from './ui/button'
+
+const NavItems = () => {
+  const [activeIndex, setActiveIndex] = useState(null)
+  return (
+    <div className='flex gap-4 h-full mr-4 items-center cursor-pointer'>
+      <Link className='navlinks' href='/dashboard'>
+        Dashboard
+      </Link>
+      <Link className='navlinks' href='/about'>
+        About
+      </Link>
+      <Link className='navlinks' href='/contact'>
+        Contact Us
+      </Link>
+      <Link className='navlinks' href='/register'>
+        Login/Register
+      </Link>
+    </div>
+  )
+}
+
+export default NavItems
